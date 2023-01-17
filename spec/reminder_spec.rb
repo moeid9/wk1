9492e8f0c2df5a27d35bed4139bcf19150fb487b
Ryan Lai <ryanlai12@gmail.com>
2023-01-17 15:17:24 +0000

    Ryan Lai -- Completed introduction for 'Testing For Errors' section

--- spec/reminder_spec.rb
+++ spec/reminder_spec.rb
@@ -6,6 +6,13 @@ describe Reminder do
       result = reminder.remind()
       expect(result).to eq "Walk the dog, Kay!"
     end
+
+    context "when no task is set" do
+      it "fails" do
+        reminder = Reminder.new("Ryan")
+        expect{ reminder.remind() }.to raise_error "No reminder set!"
+      end
+    end
     
     # We would typically have a number of these examples.
 end
