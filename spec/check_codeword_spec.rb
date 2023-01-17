b9c1a180e2d0cd3e6fabd322856ec7d9408b25b9
Moeid Haider <moeidhaider9@gmail.com>
2023-01-17 14:00:29 +0000

    Ryan Lai -- Completed Spec for Ex2

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -3,4 +3,10 @@ describe 'check_codeword' do
   it 'returns "Correct! Come in." when given "horse"' do
     expect(check_codeword('horse')).to eq "Correct! Come in."
   end
+  it 'returns "Close, but nope." when given "home"' do
+    expect(check_codeword('home')).to eq "Close, but nope."
+  end
+  it 'returns "WRONG!" when given "pass"' do
+    expect(check_codeword('pass')).to eq "WRONG!"
+  end
 end
