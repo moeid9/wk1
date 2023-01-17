d825b2584d1cfcbc8f678435252e573e5984740f
Moeid Haider <moeidhaider9@gmail.com>
2023-01-17 14:18:51 +0000

    Ryan Lai -- Introduction example

---
+++ spec/reminder_spec.rb
@@ -0,0 +1,11 @@
+require "reminder"
+describe Reminder do
+    it "reminds the user to do a task" do
+      reminder = Reminder.new("Kay")
+      reminder.remind_me_to("Walk the dog")
+      result = reminder.remind()
+      expect(result).to eq "Walk the dog, Kay!"
+    end
+    
+    # We would typically have a number of these examples.
+end
